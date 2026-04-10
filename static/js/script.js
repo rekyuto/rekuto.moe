30c974f547a42deff55ff4c97ca0238d6a9e4d85
rekyuto <62195454+rekyuto@users.noreply.github.com>
2026-04-10 16:44:43 -0700

    replaced formspree w/ google sheets

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,36 @@
+// Kafka Mouse Hover Functions
+
 function hover(img){
     img.src = "static/images/kafka/kafka4.png";
 }
 function hoverOut(img){
     img.src = "static/images/kafka/kafka1.png";
 }
+
+// AMA Form
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('ama-form');
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx0KOFQwGAzDq3dZB0UlVx1StwEjHaNhKVFcFR_k6mf4Ggu82p-BAPACKEUqO2I2llL/exec';
+
+    if (form) {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+
+            const requestBody = new FormData(form);
+
+            const submitButton = form.querySelector('input[type="submit"]');
+            const originalText = submitButton.value;
+            submitButton.value = 'sending...';
+
+            fetch(scriptURL, { method: 'POST', body: requestBody })
+                .then(response => {
+                    window.location.href = '/questions/success/';
+                })
+                .catch(error => {
+                    console.error('Error!', error.message);
+                    window.location.href = '/placeholder';
+                });
+        });
+    }
+});
